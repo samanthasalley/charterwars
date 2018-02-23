af6c6b30d9cf41f9062e3fd4cb67bd3613d4fa48
Samantha Salley <samanthasalley@gmail.com>
2018-02-23 15:00:10 -0800

    make Navbar stateful, update to use Share modal instead of direct share icons

---
+++ client/containers/Navbar.jsx
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import { Navbar, Nav, NavItem, Button } from 'react-bootstrap';
+import Share from '../components/Share';
+import cwlogo from '../assets/images/brand-white-transparent.png';
+import '../stylesheets/components/navbar.css';
+
+class CWNavbar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showShare: false
+    }
+
+    this.handleShow = this.handleShow.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+    this.generateLink = this.generateLink.bind(this);
+  }
+
+  handleClose() {
+    this.setState({ showShare: false });
+  }
+
+  handleShow() {
+    this.setState({ showShare: true });
+  }
+
+  generateLink() {
+    return (
+      this.props.location.pathname === '/' ? <Link to="/about">About</Link> : <Link to="/">Home</Link>
+    );
+  }
+
+  render() {
+    const NavLink = this.generateLink();
+
+    return (
+      <div className="cw-navbar">
+        <Navbar collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <Link to="/">
+                <img src={cwlogo} alt="Charter Wars Logo" className="brand-logo" />
+              </Link>
+            </Navbar.Brand>
+            <div className="navbar-toggle-wrapper">
+              <Navbar.Toggle className="btn csx-btn-light csx-navbar-toggle" />
+            </div>
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <ul className="navbar-nav nav navbar-right">
+              <li role="presentation" className="navbar-icon">
+                {NavLink}
+              </li>
+              <li role="presentation" className="navbar-share">
+                <Button onClick={this.handleShow}>Share</Button>
+              </li>
+            </ul>
+          </Navbar.Collapse>
+        </Navbar>
+        <Share show={this.state.showShare} handleClose={this.handleClose} />
+      </div>
+    );
+  }
+}
+
+export default withRouter(CWNavbar);
